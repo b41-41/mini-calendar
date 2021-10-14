da167c2d6b08de6ca6106cf739c6e16988500c78
b41-41 <41@b41.kr>
2021-10-14 18:56:17 +0900

    print october

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,18 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './calendar.css';
 
 const Calendar = () => {
+
+    const [month, setMonth] = useState();
+
+
+
+    const date = new Date();
+    const today = date.getDate();
+    const endDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+    const prevEndDay = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
+    date.setDate(1);
+    const startDayIndex = date.getDay();
+    const lastDayIndex = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDay();
+
+    let days = [];
+    let prevDays = [];
+    let nextDays = [];
+
+
+    const inputPrevDaysArray = () => {
+        for (let x = startDayIndex; x > 0; x--) {
+            prevDays.push(<div className="day__prev">{(prevEndDay - x) + 1}</div>)
+        }
+    }
+
+    const inputDaysArray = () => {
+        for (let i = 1; i <= endDay; i++) {
+            i === today ?
+                days.push(<div className="day__today">{i}</div>)
+                : days.push(<div>{i}</div>)
+        };
+    }
+
+    const inputNextDaysArray = () => {
+        const nextDaysLength = 6 - lastDayIndex;
+        for (let j = 1; j <= nextDaysLength; j++) {
+            nextDays.push(<div className="day__next">{j}</div>)
+        }
+    }
+
+    const init = () => {
+        inputDaysArray();
+        inputPrevDaysArray();
+        inputNextDaysArray();
+    };
+
+    init();
 
     return (
         <>
             <div className="calendar">
                 <div className="month">
                     <div className="month__current">2021.10</div>
-                    <div className="month__button">
-                        <div className="month__handle">{`<`}</div>
-                        <div className="month__handle">{`>`}</div>
-                        <div className="month__returnCurrent">이번달</div>
-                    </div>
+                    <div className="month__handle">{`<`}</div>
+                    <div className="month__handle">{`>`}</div>
+                    <div className="month__returnCurrent">이번달</div>
                 </div>
                 <div className="week">
                     <div>월</div>
@@ -24,7 +68,10 @@ const Calendar = () => {
                     <div>일</div>
                 </div>
                 <div className="day">
-                    <div className="day__prev">26</div>
+                    {prevDays.map(prevDay => prevDay)}
+                    {days.map(day => day)}
+                    {nextDays.map(nextDay => nextDay)}
+                    {/* <div className="day__prev">26</div>
                     <div className="day__prev">27</div>
                     <div className="day__prev">28</div>
                     <div className="day__prev">29</div>
@@ -51,7 +98,7 @@ const Calendar = () => {
                     <div>20</div>
                     <div>21</div>
                     <div>22</div>
-                    <div>23</div>
+                    <div className="day__select">23</div>
                     <div className="day__today">24</div>
                     <div>25</div>
                     <div>26</div>
@@ -65,7 +112,7 @@ const Calendar = () => {
                     <div className="day__next">3</div>
                     <div className="day__next">4</div>
                     <div className="day__next">5</div>
-                    <div className="day__next">6</div>
+                    <div className="day__next">6</div> */}
                 </div>
 
             </div>
