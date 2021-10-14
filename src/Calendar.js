60430bb6dc8ccec2aa6c15416c85eebc5de4dc8f
b41-41 <41@b41.kr>
2021-10-15 01:16:39 +0900

    add rerender hook

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,11 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './calendar.css';
 
 const Calendar = () => {
 
-    const [month, setMonth] = useState();
-
-
+    const [month, setMonth] = useState((new Date().getMonth()) + 1);
 
     const date = new Date();
     const today = date.getDate();
@@ -19,12 +17,11 @@ const Calendar = () => {
     let prevDays = [];
     let nextDays = [];
 
-
     const inputPrevDaysArray = () => {
         for (let x = startDayIndex; x > 0; x--) {
             prevDays.push(<div className="day__prev">{(prevEndDay - x) + 1}</div>)
         }
-    }
+    };
 
     const inputDaysArray = () => {
         for (let i = 1; i <= endDay; i++) {
@@ -32,13 +29,29 @@ const Calendar = () => {
                 days.push(<div className="day__today">{i}</div>)
                 : days.push(<div>{i}</div>)
         };
-    }
+    };
 
     const inputNextDaysArray = () => {
         const nextDaysLength = 6 - lastDayIndex;
         for (let j = 1; j <= nextDaysLength; j++) {
             nextDays.push(<div className="day__next">{j}</div>)
         }
+    };
+
+    const switchPrevMonth = () => {
+        month === 1 ?
+            setMonth(12) :
+            setMonth(month - 1);
+    };
+
+    const switchNextMonth = () => {
+        month === 12 ?
+            setMonth(0) :
+            setMonth(month + 1);
+    };
+
+    const switchCurrentMonth = () => {
+        setMonth((new Date().getMonth()) + 1);
     }
 
     const init = () => {
@@ -47,16 +60,21 @@ const Calendar = () => {
         inputNextDaysArray();
     };
 
-    init();
+    init()
+
+    useEffect(() => {
+        console.log(`month`)
+        console.log(month)
+    }, [month])
 
     return (
         <>
             <div className="calendar">
                 <div className="month">
                     <div className="month__current">2021.10</div>
-                    <div className="month__handle">{`<`}</div>
-                    <div className="month__handle">{`>`}</div>
-                    <div className="month__returnCurrent">이번달</div>
+                    <div className="month__handle" onClick={switchPrevMonth}>{`<`}</div>
+                    <div className="month__handle" onClick={switchNextMonth}>{`>`}</div>
+                    <div className="month__returnCurrent" onClick={switchCurrentMonth}>이번달</div>
                 </div>
                 <div className="week">
                     <div>월</div>
@@ -71,48 +89,6 @@ const Calendar = () => {
                     {prevDays.map(prevDay => prevDay)}
                     {days.map(day => day)}
                     {nextDays.map(nextDay => nextDay)}
-                    {/* <div className="day__prev">26</div>
-                    <div className="day__prev">27</div>
-                    <div className="day__prev">28</div>
-                    <div className="day__prev">29</div>
-                    <div className="day__prev">30</div>
-                    <div>1</div>
-                    <div>2</div>
-                    <div>3</div>
-                    <div>4</div>
-                    <div>5</div>
-                    <div>6</div>
-                    <div>7</div>
-                    <div>8</div>
-                    <div>9</div>
-                    <div>10</div>
-                    <div>11</div>
-                    <div>12</div>
-                    <div>13</div>
-                    <div>14</div>
-                    <div>15</div>
-                    <div>16</div>
-                    <div>17</div>
-                    <div>18</div>
-                    <div>19</div>
-                    <div>20</div>
-                    <div>21</div>
-                    <div>22</div>
-                    <div className="day__select">23</div>
-                    <div className="day__today">24</div>
-                    <div>25</div>
-                    <div>26</div>
-                    <div>27</div>
-                    <div>28</div>
-                    <div>29</div>
-                    <div>30</div>
-                    <div>31</div>
-                    <div className="day__next">1</div>
-                    <div className="day__next">2</div>
-                    <div className="day__next">3</div>
-                    <div className="day__next">4</div>
-                    <div className="day__next">5</div>
-                    <div className="day__next">6</div> */}
                 </div>
 
             </div>
